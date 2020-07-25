d08869c7a950bf92ea4368e864539badfb25807d
xana <xanarahmani@gmail.com>
2020-07-26 02:31:05 +0430

    Improve Coding

--- base/Utilities.py
+++ base/Utilities.py
@@ -7,22 +7,30 @@ from Crypto.Hash import SHA256
 
 
 def generate_key():
-    return RSA.generate(1024,os.urandom)
-def encrypt(message,pubKey):
+    return RSA.generate(1024, os.urandom)
+
+
+def encrypt(message, pubKey):
     encryptor = PKCS1_OAEP.new(pubKey)
     encrypted = encryptor.encrypt(message.encode("utf-8"))
     return encrypted
-def decrypt(message,keypair):
+
+
+def decrypt(message, keypair):
     decryptor = PKCS1_OAEP.new(keypair)
     decrypted = decryptor.decrypt(message)
     return decrypted
-def sign(message,keypair):
+
+
+def sign(message, keypair):
     h = SHA256.new()
     h.update(message.encode("utf_8"))
     signer = PKCS1_v1_5.new(keypair)
     signature = signer.sign(h)
     return signature
-def verify(message,signature,pubkey):
+
+
+def verify(message, signature, pubkey):
     signer = PKCS1_v1_5.new(pubkey)
     h = SHA256.new()
     h.update(message.encode("utf_8"))
