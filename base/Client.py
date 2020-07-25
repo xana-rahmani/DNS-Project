0ad8afff3434274f017cbada2acb17bb3899b7bc
xana <xanarahmani@gmail.com>
2020-07-25 20:26:11 +0430

    Add Client

---
+++ base/Client.py
@@ -0,0 +1,14 @@
+import requests
+
+session = requests.Session()
+BASE_URL = "http://127.0.0.1:8000/"
+
+
+def generateCertificaat():
+    payload = {
+        "national_code": "1055219379",
+        "name": "xana"
+    }
+    response = session.post(url=BASE_URL + "generate-certificaat", data=payload)
+    print(response.json())
+    return response
