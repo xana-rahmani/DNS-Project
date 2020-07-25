5e1d8f858ce8db03b21989fb6a64160b7a5bd239
khashiii97 <khashiii97@gmail.com>
2020-07-25 18:21:14 +0430

    second commit, main Modules built

--- Voting_System/settings.py
+++ Voting_System/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CA.apps.CaConfig',
+    'AS.apps.AsConfig',
+    'VS.apps.VsConfig',
 ]
 
 MIDDLEWARE = [
