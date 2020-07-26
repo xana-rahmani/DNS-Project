d6e7e104ca1c038f5af0d86d4f3fb659c327ef7a
xana <xanarahmani@gmail.com>
2020-07-26 16:34:25 +0430

    Add load key to Client

--- base/Client.py
+++ base/Client.py
@@ -1,5 +1,17 @@
 import requests
-from Voting_System.settings import load_public_key
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+Public_Keys_DIR = os.path.join(BASE_DIR, 'Public-Keys')
+
+
+def load_public_key(path):
+    print("load_public_key")
+    path = os.path.join(Public_Keys_DIR, path)
+    with open(path, 'r') as f:
+        key = f.read()
+    return key
+
 
 session = requests.Session()
 BASE_URL = "http://127.0.0.1:8000/"
