8d21ec007f110b32fbcc1bd16740084827f04355
xana <xanarahmani@gmail.com>
2020-07-26 16:55:08 +0430

    Improve view

--- CA/views.py
+++ CA/views.py
@@ -13,21 +13,31 @@ def generate_certificaat(request):
     try:
         name = request.POST["name"]
         national_code = request.POST["national_code"]
+        if national_code is None or name is None:
+            return JsonResponse({'status': 'fail', 'message': 'نام یا کدملی به درستی ارسال نشده است.'}, status=200)
     except:
         return JsonResponse({'status': 'fail', 'message': 'نام یا کدملی به درستی ارسال نشده است.'}, status=200)
 
     try:
         # print('load_key', load_key('CA-private.key'))
-        user = User.objects.filter(name=name, national_code=national_code)[0]
+        user = User.objects.filter(name=name, national_code=national_code)
+        if user.count() <= 0:
+            return JsonResponse({'status': 'fail', 'message': 'نام و کدملی شما معتبر نیست.'}, status=200)
+        elif user.count() > 1:
+            print("#ERROR IN SYSTEM: 2 user with same national code")
+            return JsonResponse({'status': 'fail', 'message': 'لطفا با پشتیبانی تماس بگیرید.'}, status=200)
+
+        user = User.objects.filter(name=name, national_code=national_code).first()
         if user:
-            certificaat = Certificaat.objects.filter(user=user)[0]
-            if certificaat:
+            certificaat = Certificaat.objects.filter(user=user)
+            if certificaat.count() == 1:
+                certificaat = certificaat.first()
                 private_key = certificaat.private_key
                 public_key = certificaat.public_key
                 return JsonResponse(
                     {
                         'status': 'successful',
-                        'message': 'گواهی شما.',
+                        'message': 'گواهی شما ارسال شد.',
                         'private_key': private_key,
                         'public_key': public_key
                     },
@@ -43,10 +53,10 @@ def generate_certificaat(request):
                         'public_key': public_key
                     },
                     status=200)
-
         else:
-            return JsonResponse({'status': 'fail', 'message': 'نام و کدملی شما معتبر نیست.'}, status=200)
-    except:
+            return JsonResponse({'status': 'fail', 'message': 'لطفا بعدا تلاش کنید.'}, status=200)
+    except Exception as e:
+        print("#Exception: {}".format(e))
         return JsonResponse({'status': 'fail', 'message': 'لطفا بعدا تلاش کنید.'}, status=200)
 
 
