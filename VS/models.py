1f81709f4659f9e27be69735c1d69914c8f86034
khashiii97 <khashiii97@gmail.com>
2020-07-29 00:33:10 +0430

    models for VS

--- VS/models.py
+++ VS/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Candidates(models.Model):
+    number_of_votes = models.IntegerField(default=0)
+class Votes(models.Model):
+    public_key = models.CharField(max_length=100, unique=True)
+    voted_to = models.ForeignKey(Candidates, on_delete=models.CASCADE)
