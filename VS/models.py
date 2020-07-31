a2eee5ba7b39d5735f194df1d60ffe129bb2d9f1
xana <xanarahmani@gmail.com>
2020-08-01 03:34:19 +0430

    Add vote

--- VS/models.py
+++ VS/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Candidates(models.Model):
+    candidate_id = models.CharField(max_length=100, unique=True)
     number_of_votes = models.IntegerField(default=0)
+
+
 class Votes(models.Model):
     public_key = models.CharField(max_length=100, unique=True)
     voted_to = models.ForeignKey(Candidates, on_delete=models.CASCADE)
--- VS/views.py
+++ VS/views.py
@@ -1,14 +1,14 @@
 import os
 import json
-from datetime import datetime
 import base64
 from django.http.response import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-from .models import Candidates,Votes
+from .models import Candidates, Votes
 from base import Utilities
 from Voting_System import settings
 from Crypto.PublicKey import RSA
+from django.db import IntegrityError, transaction
 
 
 def load_RSA_key(path):
@@ -43,9 +43,10 @@ def vote(request):
         user_vote = Utilities.payload_decryptor_Fernet(vote_data, vote_crt_sk_voter.encode())
 
         """ Check AS Certification """
-        print("-- Check AS Certification")
         if checkAsCertificationSignature(sk_voter=vote_crt_sk_voter, public_key=vote_crt_user_public_key, signature=vote_crt_AS_signature):
-            print("Xana 74938248")
+            payload = {'status': 'fail', 'message': 'امضای گواهی AS معتبر نمی‌باشد.'}
+            return sendResponse(payload, sessionKey)
+
         """ Check Vote """
         if vote_crt_status != "successful":
             payload = {'status': 'fail', 'message': 'مشکلی در حالت توکن ارسالی وجود دارد.'}
@@ -53,6 +54,10 @@ def vote(request):
         if not checkVoteSignature(user_vote=user_vote, pubkey=vote_crt_user_public_key):
             payload = {'status': 'fail', 'message': 'امضای رای داده شده با کلید عمومی موجود در توکن مطابقت ندارد.'}
             return sendResponse(payload, sessionKey)
+
+        payload = addVote(candidate_id=user_vote.get("vote"), public_key=vote_crt_user_public_key)
+        return sendResponse(payload, sessionKey)
+
     except Exception as e:
         print("#Exception-1: {}".format(e))
         payload = {'status': 'fail', 'message': 'درخواست به درستی ارسال نشده است.'}
@@ -80,6 +85,40 @@ def checkAsCertificationSignature(sk_voter, public_key, signature):
                 )
     except Exception as e:
         return False
+
+
+def addVote(candidate_id, public_key):
+
+    """ Find  candidate """
+    candidate = Candidates.objects.filter(candidate_id=candidate_id)
+    if candidate.count() <= 0:
+        return {'status': 'fail', 'message': 'کاندیدای با این آیدی وجود ندارد.'}
+    elif candidate.count() == 1:
+        candidate = candidate.first()  # Candidate Found
+    else:
+        print("#ERROR IN SYSTEM: 2 candidate with same candidate_id")
+        return {'status': 'fail', 'message': 'لطفا با پشتیبانی تماس بگیرید.'}
+
+    """ Add Vote for Candidate """
+    try:
+        user_vote = Votes.objects.filter(public_key=public_key)
+        if user_vote.count() != 0:
+            user_vote = user_vote.first()
+            voted_to = user_vote.voted_to
+            message = 'شما قبلا به {} رای داده‌اید.'.format(voted_to.candidate_id)
+            return {'status': 'fail', 'message': message}
+        elif user_vote.count() == 0:
+            with transaction.atomic():
+                newVote = Votes(public_key=public_key, voted_to=candidate)
+                newVote.save()
+                temp_number_of_vote = candidate.number_of_votes
+                candidate.update(number_of_votes=temp_number_of_vote + 1)
+                message = 'رای شما به کاندیدا با شماره {} ثبت گردید.'.format(candidate.candidate_id)
+                return {'status': 'fail', 'message': message}
+    except IntegrityError:
+        print("#ERROR IN SYSTEM: error in add Vote in Data Base")
+        return {'status': 'fail', 'message': 'لطفا با پشتیبانی تماس بگیرید.'}
+    return {'status': 'fail', 'message': 'لطفا با پشتیبانی تماس بگیرید.'}
 
 
 def sendResponse(data, key):
